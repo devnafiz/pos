4ba9f70e33cd8a4eda0897f8c95e13c615fd8de1
devnafiz <nafiz.2011@yahoo.com>
2020-12-01 21:17:44 +0600

    first commit

---
+++ app/Http/Middleware/Test.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class Test
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            if(Auth::user()->usertype=='Admin'){
+
+            }elseif (Auth::user()->usertype=='User') {
+                # code...
+            }
+            //return $next($request);
+
+        }else{
+            return redirect('/login');
+            //return redirect()->back();
+
+        }
+        
+    }
+}
